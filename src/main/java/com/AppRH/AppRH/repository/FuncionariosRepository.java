5f2b93a39733ab4d2077e15969eebe34abad065d
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-01-13 20:08:13 -0300

    feat: add interface FuncionariosRepository with personal methods personalized

--- src/main/java/com/AppRH/AppRH/repository/FuncionariosRepository.java
+++ src/main/java/com/AppRH/AppRH/repository/FuncionariosRepository.java
@@ -4,4 +4,6 @@ import com.AppRH.AppRH.model.Funcionarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FuncionariosRepository extends JpaRepository<Funcionarios,String> {
+    Funcionarios findById(long id);
+    Funcionarios findByNome(String nome);
 }
