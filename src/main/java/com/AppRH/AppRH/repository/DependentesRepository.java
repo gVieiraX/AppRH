022fabc5a9ea0cc22403e5f7f1302837a6c47502
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-01-14 21:36:39 -0300

    feat: add repository methods for dependent entity queries

--- src/main/java/com/AppRH/AppRH/repository/DependentesRepository.java
+++ src/main/java/com/AppRH/AppRH/repository/DependentesRepository.java
@@ -1,7 +1,14 @@
 package com.AppRH.AppRH.repository;
 
 import com.AppRH.AppRH.model.Dependentes;
+import com.AppRH.AppRH.model.Funcionarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DependentesRepository extends JpaRepository<Dependentes,String> {
+import java.util.List;
+
+public interface DependentesRepository extends JpaRepository<Dependentes, String> {
+    Iterable<Dependentes> findByFuncionarios(Funcionarios funcionarios);
+    Dependentes findByCpf(String cpf);
+    Dependentes findById(long id);
+    List<Dependentes> findByNome(String nome);
 }
